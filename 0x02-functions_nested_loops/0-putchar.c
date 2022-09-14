a0f1774bd3c27d42b0877a90011863b20999af4f
haddiee <rajihadiyah.rh@gmail.com>
2022-09-13 20:11:14 -0700

    Functions task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* main - Prints putchar
+*
+* Return: End program
+*/
+
+int main(void)
+
+{
+char string[8] = "_putchar";
+
+	int n = 0;
+
+
+
+	while (n < 8)
+
+	{
+
+		_putchar(string[n]);
+
+		++n;
+
+	}
+
+
+
+	_putchar(10);
+
+	return (0);
+
+}
